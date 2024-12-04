add2cfc11b0e71a3a9c481874e61bdd0513616f8
Ziegel <skvorcov-pa-vel@yandex.ru>
2024-12-04 18:25:26 +0300

    Тут появилась информация про новый год

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def print_hi():
     # Use a breakpoint in the code line below to debug your script.
     print('Паша молодец')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Скоро новый год!')
 
 
 # Press the green button in the gutter to run the script.
